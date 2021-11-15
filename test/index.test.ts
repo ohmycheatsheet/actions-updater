fe6b774d06161c1f08884875499015a348e24d13
JiangWeixian <jiangweixian1994@gmail.com>
2021-11-15 13:49:39 +0800

    :white_check_mark:  update todo

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,7 @@
 describe('main', () => {
   it.todo('fetch latest cheatsheet template should work')
   it.todo('update with latest cheatsheet template should work')
+  it.todo('read changelog should')
+  it.todo('read version should work')
+  it.todo('same version should skip')
 })
